bc8d33cca25dd9e0beac3e9e4f6a319e1ca543b8
Achille Pénet <achille.penet@icloud.com>
2017-11-27 21:01:17 +0100

    Server first implementation

---
+++ Server/SOCKETCLIENT/socketclient.js
@@ -0,0 +1,18 @@
+// content of index.js
+const http = require('http')
+const port = 8083
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello SocketClient Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
