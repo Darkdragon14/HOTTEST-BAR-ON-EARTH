4dca7920c58e5d1d4907da3a4a22a257d160aac0
Achille Pénet <achille.penet@icloud.com>
2017-11-28 19:00:03 +0100

    Last error ...

--- Server/AUTHAPP/authapp.js
+++ Server/AUTHAPP/authapp.js
@@ -1,5 +1,4 @@
 // content of index.js
-<<<<<<< HEAD
 const port = 8080
 
 const app = require('express')()
@@ -15,22 +14,4 @@ app.get('/register/', function(req, res){
 
 app.listen(port, function(){
   console.log('listening on : ' + port)
-=======
-const http = require('http')
-const port = 8080
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello AuthApp Server!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
->>>>>>> bc8d33cca25dd9e0beac3e9e4f6a319e1ca543b8
 })
--- Server/SOCKETBAR/socketbar.js
+++ Server/SOCKETBAR/socketbar.js
@@ -1,5 +1,4 @@
 // content of index.js
-<<<<<<< HEAD
 const port = 8082
 
 const app = require('express')()
@@ -17,22 +16,4 @@ app.get('/connectBar/', function(req, res){
 
 app.listen(port, function(){
   console.log('listening on : ' + port)
-=======
-const http = require('http')
-const port = 8082
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello SocketBar Server!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
->>>>>>> bc8d33cca25dd9e0beac3e9e4f6a319e1ca543b8
 })
