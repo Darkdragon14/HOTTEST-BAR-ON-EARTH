9cb452aa12a2b118b53a3934cd56aded4373e347
Sinoquet Tristan <s.tristan@hotmail.fr>
2017-12-09 17:56:54 +0100

    Add test Ok

--- recommandation/app.js
+++ recommandation/app.js
@@ -23,28 +23,33 @@ var dataRecoWithLive = {
   	method: 'POST',
 }
 
+//Pour un poid plus fort d'un valeur il faut répéter l'élément plusieurs fois
+var fields = [
+  	{ name: "temperature", measure: nn.comparisonMethods.number, max: 50 },
+  	{ name: "bar", measure: nn.comparisonMethods.word }, 
+  	{ name: "musique", measure: nn.comparisonMethods.word },
+  	{ name: "occupation", measure: nn.comparisonMethods.number, max: 500 },
+];
 
 /****************************************************************
 Lorsqu'un client se connect à l'application
 ****************************************************************/
 app.get('/newClient/:IDUser&:musique&:bar&:occupation&:temperature', function(req, res){
-    var query = {IDUser: req.params.IDUser, musique: req.params.musique, bar: req.params.bar, occupation: req.params.occupation, temperature: req.params.temperature};
-    //Pour un poid plus fort d'un valeur il faut répéter l'élément plusieurs fois
-    var fields = [
-	  	{ name: "temperature", measure: nn.comparisonMethods.number, max: 50 },
-	  	{ name: "bar", measure: nn.comparisonMethods.word }, 
-	  	{ name: "musique", measure: nn.comparisonMethods.word },
-	  	{ name: "occupation", measure: nn.comparisonMethods.number, max: 500 },
-	];
+    var query = {IDUser: req.params.IDUser, temperature: parseInt(req.params.temperature), bar: req.params.bar, musique: req.params.musique, occupation: parseInt(req.params.occupation)};
+    
     res.send("Ok");
     http.get(dataLive, function(resp){
 		resp.setEncoding('utf8');
 		resp.on('data', function (chunk) {
-		    nn.findMostSimilar(query, chunk, fields, function(nearestNeighbor, probability) {
-			  	console.log(query);
+			var items = JSON.parse(chunk)
+			console.log(query);
+			console.log(fields);
+		    nn.findMostSimilar(query, items, fields, function(nearestNeighbor, probability) {
+			  	//console.log(query);
 			  	console.log(nearestNeighbor);
 			  	console.log(probability);
-			  	dataRecoWithLive.json = [{IDUser: query.IDUser, IDBar: nearestNeighbor.name, Probability: probability}];
+			  	console.log([{IDUser: query.IDUser, IDBar: nearestNeighbor.name, Probability: probability}]);
+			  	//dataRecoWithLive.json = [{IDUser: query.IDUser, IDBar: nearestNeighbor.name, Probability: probability}];
 				/*request(dataRecoWithLive, function (error, response, body) {
 				  	if (!error && response.statusCode == 200) {
 				    	console.log(body.id) // Print the shortened url.
