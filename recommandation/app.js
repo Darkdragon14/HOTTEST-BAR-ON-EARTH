84cec5179b0b41756ff1f24c9ba7d12a3f20c39b
lhipployte <hippolyte.lacassagne@gmail.com>
2018-01-29 15:47:18 +0100

    last change

--- recommandation/app.js
+++ recommandation/app.js
@@ -8,9 +8,11 @@ const server = http.createServer(app);
 const nn = require('nearest-neighbor');
 const request = require('request');
 
+//adresse à modifier si nécessaire 
 const host = 'localhost';
 const port = 3000;
 
+//les différentes requêtes
 const dataLive = {
   hostname: host,
   port: port,
@@ -29,6 +31,13 @@ const recupUsers = {
 	hostname: host,
 	port: port,
 	path: '/allUser',
+	method: 'GET',
+}
+
+const recupDonneeBar = {
+	hostname: host,
+	port: port,
+	path: '/dataBar',
 	method: 'GET',
 }
 
@@ -53,7 +62,7 @@ app.get('/newClient/:IDUser&:musique&:bar&:occupation&:temperature', function(re
 		    nn.findMostSimilar(query, items, fields, function(nearestNeighbor, probability) {
 			  	var options = { 
 			  		method: 'POST',
-				  	url: 'http://localhost:port/sendRecommandations',
+				  	url: "http://"+host+":"+port+"/sendRecommandations",
 				  	headers: {'Content-Type': 'application/json'},
 				  	body: { IDUser: query.IDUser, IDBar: nearestNeighbor.IDBar, Probability: probability },
 				  	json: true 
@@ -72,8 +81,12 @@ app.get('/newClient/:IDUser&:musique&:bar&:occupation&:temperature', function(re
 Calcul journalier
 ****************************************************************/
 CalculRecommendation();
+CalculRecommendationV2();
 setInterval(CalculRecommendation, 3600000);
 
+/****************************************************************
+Prise en compte de ces préférences
+****************************************************************/
 function CalculRecommendation(){
 	var user = new Array();
 	var avis = new Array();
@@ -107,10 +120,10 @@ function CalculRecommendation(){
 						bar = avisTri[i];
 					}
 					if(i == avisTri.length - 1){
-						probability = probability * ((bar.note * 20)/100);
+						probability = probability * (bar.note * 0.2);
 						var options = { 
 					  		method: 'POST',
-						  	url: 'http://localhost:port/sendRecommandations',
+						  	url: "http://"+host+":"+port+"/sendRecommandations",
 						  	headers: {'Content-Type': 'application/json'},
 						  	body: { IDUser: query.IDUser, IDBar: bar.IDBar, Probability: probability },
 						  	json: true 
@@ -121,6 +134,96 @@ function CalculRecommendation(){
 					}
 				}
 			});
+		}
+	}, 5000);
+}
+
+/****************************************************************
+Système avec auto apprentissage des préférences utilisateur
+par rapport aux notes qu'il a mises précédement.
+****************************************************************/
+function CalculRecommendationV2(){
+	var avis = new Array();
+	var donneeBar = new Array();
+	http.get(recupAvis, function(resp){
+	resp.setEncoding('utf8');
+		resp.on('data', function (chunk) {
+			avis = JSON.parse(chunk);
+		});
+	}).on("error", function(e){
+  		console.log("Got error: " + e.message);
+	});
+	http.get(recupDonneeBar, function(resp){
+	resp.setEncoding('utf8');
+		resp.on('data', function (chunk) {
+			donneeBar = JSON.parse(chunk);
+		});
+	}).on("error", function(e){
+  		console.log("Got error: " + e.message);
+	});	
+	//attente pour être sur d'avoir toutes les données
+	setTimeout(function(){
+		var userFait = new Array();
+		var avisTri = new Array();
+		var paramUser = new Array();
+		for(var i = 0; i < avis.length; i++){
+			if(userFait.indexOf(avis[i].IDUser) == -1){
+				userFait.push(avis[i].IDUser);
+				avisTri[avisTri.length] = new Array();
+			}
+			avisTri[userFait.indexOf(avis[i].IDUser)].push({IDBar: avis[i].IDBar, note: avis[i].note, date: avis[i].date});
+			if(i == avis.length - 1){
+				var paramUser = new Array();
+				for(var j = 0; j < userFait.length; j++){
+					avisTri[j].sort(function(a,b){
+						return a.note - b.note;
+					});
+					var valSup;
+					var count = 0;
+					for(var k = avisTri[j].length-1; k > -1; k--){
+						if(k == avisTri[j].length-1){
+							valSup = avisTri[j][k].note;
+							count++;
+						}
+						else{
+							if(valSup == avisTri[j][k].note){
+								count++;
+							}
+						}
+						if(k == 0){
+							count = avisTri[j].length - count;
+							avisTri[j] = avisTri[j].slice(count);
+							for(var l = 0; l < avisTri[j].length; l++){
+								var tmp = donneeBar.find(donneeBar => avisTri[j][l].IDBar == donneeBar.IDBar && avisTri[j][l].date == donneeBar.date);
+								tmp.IDUser = userFait[j];
+								tmp.note = avisTri[j][l].note;
+								paramUser.push(tmp);
+							}
+							if(j == userFait.length-1){
+								var x = 0;
+								for(var m = 0; m < paramUser.length; m++){
+									var query = paramUser[m];
+									var items = paramUser.slice();
+									items = items.filter(items => items.IDUser != paramUser[m].IDUser);
+									nn.findMostSimilar(query, items, fields, function(nearestNeighbor, probability) {
+										probability = probability * (nearestNeighbor.note * 0.2);
+										var options = { 
+									  		method: 'POST',
+										  	url: "http://"+host+":"+port+"/sendRecommandations",
+										  	headers: {'Content-Type': 'application/json'},
+										  	body: { IDUser: query.IDUser, IDBar: nearestNeighbor.IDBar, Probability: probability },
+										  	json: true 
+										};
+										request(options, function (error, response, body) {
+										  	if (error) throw new Error(error);
+										});
+									});
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}, 5000);
 }
